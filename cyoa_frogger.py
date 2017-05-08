c48690c0305cb02554eb8cbb69c2abbc00aac6d1
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-05-07 18:09:40 -0700

    added to sad matrix, lifepoints still in progress

--- cyoa_frogger.py
+++ cyoa_frogger.py
@@ -15,11 +15,7 @@ def intro_choice():
 	choice = raw_input(" 1 or 2? ")
 	choice = int(choice)
 
-	# happy begining or darker begining 
-	# if choice < 2:
-	# 	print "The sun was shining happy dumb rainbow colored frogs everywhere. Then pink frog, your current mate,  came up to you and said 'I am bored of all this rainbow glitz lets GTFO.'"
-	# else: 
-	# 	print "You were dumped for a faster brighter frog that loved water. This filled you with little froggy rage. You decide its a great idea to hunt your girlfriend and your competition down. You want to explain to them in a series of hops and jumps how stupid  they are."
+	
 	# for choice should  keep it like this or should i separate it into another function?
 	# will this allow me to use this  choice later?
 	return choice
@@ -57,15 +53,45 @@ def happy_matrix():
 		x,y = movement(x,y,2,2)
 
 def sad_matrix():
-	row_x = randint(0,3)
-	col_y = randint(0,3)
-	sad_mat =  [["what", "is", "happening"],["hello", "there", " !! "],["good ", "bye", "farewell"]]
-	print sad_mat[row_x][col_y] # this is finding the location and printing it
+	lifepoints = 5
+	puddle = False
+	# row_x = randint(0,3)
+	# col_y = randint(0,3)
+	# sad_mat =  [["what", "is", "happening"],["hello", "there", " !! "],["good ", "bye", "farewell"]]
+	# print sad_mat[row_x][col_y] # this is finding the location and printing it
 	#print "this works!"
+	sad_mat = [[{"text":"stuff1", "puddle": False},{"text": "stuff 2", "puddle": False},{"text":"stuff3", "puddle": False}],
+	[{"text":"stuff4", "puddle": False},{"text":"stuff5", "puddle": False},{"text": "stuff 6", "puddle": False}],
+	[{},{},{}]]
+	for i in range(4):
+		x = randint(0,3)
+		y = randint(0,3)
+		sad_mat[x][y]["puddle"] = True
+
+	if puddle == True and lifepoints != 0:
+		lifepoints -= 1
+		print  "you have" + lifepoints + " life points left."
+	elif puddle == False and lifepoints != 0:
+		print  "you have" + lifepoints + " life points left."
+	elif lifepoints == 0:
+		print "game over"
+	else:
+		print "something went wrong" 
+
+
+"""
+What I want to happen here is I want the user to have a starting life of 5 points
+and every time puddle is true i want life points to go down by 1  and when it hits zero its game over.
+not sure how to connect  it.
+"""
 
 
 
-#calls
+
+		
+
+
+	#calls
 
 def calls():
 	display_intro()
